99a71cf545f3619f21d6bea5f5708743c75eb1ef
Gammaglobulino <mazzantia@hotmail.com>
2019-09-12 17:26:34 +0200

    Initial commit

---
+++ src/main/java/com/thedigibromeetsspark/personality_test_service/PersonalityTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thedigibromeetsspark.personality_test_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalityTestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalityTestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thedigibromeetsspark/personality_test_service/PersonalityTestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thedigibromeetsspark.personality_test_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalityTestServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
